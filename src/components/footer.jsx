103a8bb7c3a41882b748347442788ead5ef05f36
chiefcadet <ravindersh@iitbhilai.ac.in>
2023-01-15 21:20:08 +0530

    Added styling and basic components

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function footer() {
+    return (
+    <footer>
+    <p>
+    Copyright © {new Date().getFullYear()}
+    </p>
+    </footer>);
+}
+
+export default footer
